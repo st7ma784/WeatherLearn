b349be09cd2bf3961bb47c7702b77a5eafea07f1
steve <you@example.com>
2025-03-31 08:48:36 +0000

    fx

--- PTL/launch.py
+++ PTL/launch.py
@@ -141,6 +141,7 @@ def SLURMEval(ModelPath,config):
     else:
 
         sub_commands.extend(['#SBATCH -p gpu-medium',
+                             '#SBATCH --mem=96G',
                              'export CONDADIR=/storage/hpc/46/manders3/conda4/open-ce',
                              'export NCCL_SOCKET_IFNAME=enp0s31f6',])
     sub_commands.extend([ '#SBATCH --{}={}\n'.format(cmd, value) for  (cmd, value) in slurm_commands.items()])
