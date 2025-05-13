44d147189e2cb802058089d01df188e6ac2fb35f
Steve Mander <s.mander3@lancaster.ac.uk>
2025-05-13 14:04:08 +0100

    add inits

---
+++ storageServer/__init__.py
@@ -0,0 +1,12 @@
+"""
+storageServer Package
+
+This package provides functionality for managing storage operations, including interactions with MinIO, 
+file handling, and data processing.
+
+Modules:
+    - MinioToDisk: Handles downloading data from MinIO buckets to local storage.
+    - DiskToMinio: Handles uploading data from local storage to MinIO buckets.
+    - utils: Contains utility functions for storage operations.
+"""
+
